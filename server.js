681aa14a618cea098e11c4b741ff0dc9977df00c
Thammi <thammi@chaossource.net>
2010-08-24 18:58:01 +0200

    Fixing port typo

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http'), port = process.env.PORT || 8001
 http.createServer(function (req, res) {
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
 	    res.end('Hello World\n');
-}).listen(PORT, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:'+PORT+'/');
+}).listen(port, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:'+port+'/');
 
