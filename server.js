1fc482bad37ca90ba29d93dd6fab0bb75d08e16f
Thammi <thammi@chaossource.net>
2010-08-24 18:53:49 +0200

    Setting the right port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-var http = require('http');
+var http = require('http'), port = process.env.PORT || 8001
 http.createServer(function (req, res) {
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
 	    res.end('Hello World\n');
-}).listen(8124, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8124/');
+}).listen(PORT, "127.0.0.1");
+console.log('Server running at http://127.0.0.1:'+PORT+'/');
 
