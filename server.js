d4ec6071e59e13f13d4f32bca956b0a7a0a195f6
Thammi <thammi@chaossource.net>
2010-08-24 19:02:11 +0200

    Listening to the world

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ var http = require('http'), port = process.env.PORT || 8001
 http.createServer(function (req, res) {
 	  res.writeHead(200, {'Content-Type': 'text/plain'});
 	    res.end('Hello World\n');
-}).listen(port, "127.0.0.1");
+}).listen(parseInt(port));
 console.log('Server running at http://127.0.0.1:'+port+'/');
 
