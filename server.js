90526520cdd72474f9d0a9241c80a90dc0c41127
Thammi <thammi@chaossource.net>
2010-09-05 02:20:45 +0200

    Applying AGPLv3

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
+/*
+ * hello unicorn - Your way to amazing unicorns
+ *
+ * Copyright (C) 2010 Thammi, dodo, rbjl
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 var http = require('http'),
     querystring = require('querystring'),
     port = process.env.PORT || 8001
@@ -75,3 +94,4 @@ http.createServer(function (req, res) {
 }).listen(parseInt(port));
 
 console.log('Server running at http://127.0.0.1:'+port+'/');
+
